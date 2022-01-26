5c7a4efbfab8aafa034c9e2e9f6965186a24ee4a
DIMITRIOS KALAITZIS <d.kalaitzis@yahoo.com>
2022-01-26 21:33:52 +0000

    MoviePageStyling

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useState } from "react/cjs/react.development";
+import { BiArrowBack } from "react-icons/bi";
 
 const MoviePage = (props) => {
   const { imbdID } = useParams();
@@ -27,18 +28,41 @@ const MoviePage = (props) => {
   }, []);
 
   return (
-    <div className="container details-page">
-      <div className="movie-img">
-        <img
-          src={movie.Poster}
-          alt={movie.imdbID}
-          width="100"
-          height="120"
-        ></img>
-      </div>
-      <div className="description-box">
-        <div className="row">
-            hi
+    <div className="container-fluid movie-page">
+      <Link to={`/`} className="btn back-button"> <BiArrowBack /> Back </Link>
+      <div className="col movie-container">
+        <div className="movie-img">
+          <img
+            src={movie.Poster}
+            alt={movie.imdbID}
+            width="400"
+            height="500"
+          ></img>
+        </div>
+        <div className="col description-box">
+          <div className="row title">
+            <h1> {movie.Title} </h1>
+          </div>
+          <div className="row plot">
+            <h3> Movie Plot </h3>
+            <p> {movie.Plot} </p>
+          </div>
+          <div className="row actors">
+            <h3> Actors </h3>
+            <p> {movie.Actors} </p>
+          </div>
+          <div className="row genre">
+            <h3> Genre </h3>
+            <p> {movie.Genre} </p>
+          </div>
+          <div className="row language">
+            <h3> Language </h3>
+            <p> {movie.Language} </p>
+          </div>
+          <div className="row rating">
+            <h3> IMDB Rating </h3>
+            <p> {movie.imdbRating} </p>
+          </div>
         </div>
       </div>
     </div>
